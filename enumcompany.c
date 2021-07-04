e6c25babe92f4299fc9a33d85d3db19fc01e3aa0
Yojita <yojita98@gmail.com>
2021-07-04 15:55:44 +0530

    Address code review comments
    Corrected code formatting as per the coding standards.

--- enumcompany.c
+++ enumcompany.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
-enum company {GOOGLE,FACEBOOK,XEROX,YAHOO = 10,EBAY,MICROSOFT};
-int main(){
+#include <stdio.h>  
+
+enum company {GOOGLE, FACEBOOK, XEROX, YAHOO = 10, EBAY, MICROSOFT};
+
+int main() {
     enum company xerox = XEROX;
     enum company google = GOOGLE;
     enum company ebay = EBAY;
-    printf("value assigned to xerox is %d\n",xerox);
-    printf("value assigned to xerox is %d\n",google);
-    printf("value assigned to xerox is %d\n",ebay);
-    
-    
+    printf("value assigned to xerox is %d\n", xerox);
+    printf("value assigned to xerox is %d\n", google);
+    printf("value assigned to xerox is %d\n", ebay);
+    return 0;
 }
