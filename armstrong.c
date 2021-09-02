86fd58f3dc47f289e3cacd3032087f1c954e7f73
yojita <yojita98@gmail.com>
2021-09-02 13:01:15 +0530

    created program to display armstrong numbers between 1 to 1000

---
+++ armstrong.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int num, result, sum, i;
+    printf("Armstrong numbers between 1 to 1000 are : \n");
+    
+    for(i = 1; i <= 1000; i++)
+    {
+        num = i;
+        sum = 0;
+        while(num != 0)
+        {
+            result = num % 10;
+            sum = sum + ( result * result * result);
+            num = num /10;
+        }
+        if(sum == i)
+        printf("%d\n", i);
+    }
+}
