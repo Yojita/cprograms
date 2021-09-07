56ebd81ab741703b0c60782cba07df4b646789e3
yojita <yojita98@gmail.com>
2021-09-07 20:17:32 +0530

    Program to accept a number from user and calculate sum of even digits of a given number

---
+++ evensum.c
@@ -0,0 +1,26 @@
+/* Write a program to accept a number from user and calculate sum of even digits of a given numbers */
+
+#include <stdio.h>
+int main()
+{
+    int num, remainder, sum;
+
+    printf("Enter the number : ");
+    scanf("%d", &num);
+
+    printf("The sum of even digits of the given number %d is : ", num);
+
+    sum = 0;
+
+    while (num > 0)
+    {
+
+        remainder = num % 10;
+        num = num / 10;
+
+        if ((remainder % 2) == 0)
+            sum = sum + remainder;
+    }
+    printf("%d\n", sum);
+    return 0;
+}
