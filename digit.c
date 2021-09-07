cb83a61d77967dbf738a5a04a050c86aa46303fd
yojita <yojita98@gmail.com>
2021-09-07 19:27:16 +0530

    Program to find sum of digits of a given numbers till it reduces to a single digit

---
+++ digit.c
@@ -0,0 +1,27 @@
+/* Write a program to to find the sum of digits of a given number till it reduces to single digit */
+
+#include <stdio.h>
+int main()
+{
+    int num, remainder, singleDigit;
+
+    printf("Enter the number : ");
+    scanf("%d", &num);
+
+    printf("The reduced form of given digit %d is : ", num);
+
+    while(num > 9)
+    {
+        singleDigit = 0;
+        while(num != 0)
+        {
+            remainder = num % 10;
+            num = num / 10;
+            singleDigit = singleDigit + remainder;
+        }
+        num = singleDigit;
+    }
+    printf("%d\n", singleDigit);
+    return 0;
+
+}
