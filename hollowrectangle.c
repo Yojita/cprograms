789038514892572eb6a5825309c308b57ba82d45
yojita <yojita98@gmail.com>
2021-09-13 11:59:26 +0530

    Program to print star pattern hollow rectangle

---
+++ hollowrectangle.c
@@ -0,0 +1,28 @@
+/* Write a program to print star pattern hollow rectangle */
+
+#include <stdio.h>
+int main()
+{
+    int num, rows, columns, i, j;
+    printf("Enter the number of rows : ");
+    scanf("%d", &rows);
+    printf("Enter the number of columns : ");
+    scanf("%d", &columns);
+
+    for(i = 0; i <= rows; i++)
+    {
+        for(j = 0; j <= columns; j++)
+        {
+            if(i == 0 || i == rows || j == 0 || j == columns)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
