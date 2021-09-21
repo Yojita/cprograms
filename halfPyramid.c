4bbf3c17cf270ddc62b1d231a7ba448724d65ff1
yojita <yojita98@gmail.com>
2021-09-21 15:31:37 +0530

    Write a program to print half pyramid pattern using stars

---
+++ halfPyramid.c
@@ -0,0 +1,20 @@
+/* Write a program to print half pyramid pattern using stars */
+
+#include <stdio.h>
+int main()
+{
+    int rows, i, j;
+
+    printf("Enter the number of rows : ");
+    scanf("%d", &rows);
+
+    for(i = 1; i <= rows; i++)
+    {
+        for(j = 1; j <= i; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
