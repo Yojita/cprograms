9a0b7a3395b286b20f0fddae3be00c7f724295b7
yojita <yojita98@gmail.com>
2021-09-21 16:05:29 +0530

    Write a program to print inverted pyramid pattern using star

---
+++ invertedpyramid.c
@@ -0,0 +1,24 @@
+/* Write a program to print inverted pyramid pattern using star */
+
+#include <stdio.h>
+int main()
+{
+    int i, j, space, rows;
+
+    printf("Enter the number of rows : ");
+    scanf("%d", &rows);
+
+    for(i = rows; i >= 1; i--)
+    {
+        for(space = 1; space <= (rows - i); space++)
+        {
+            printf(" ");
+        }
+        for(j = 1; j <= i; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
