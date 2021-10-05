df90a3c5954b018a89e007948d6de1047fd2b188
yojita <yojita98@gmail.com>
2021-10-05 16:01:27 +0530

    Write a program to print inverted half pyramid pattern using numbers

---
+++ pattern5.c
@@ -0,0 +1,20 @@
+/* Write a program to print inverted half pyramid pattern using numbers */
+
+#include <stdio.h>
+int main()
+{
+    int i, j, rows;
+
+    printf("Enter the number of rows : ");
+    scanf("%d", &rows);
+
+    for(i = rows; i >= 1; i--)
+    {
+        for(j = 1; j <= i; j++)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
