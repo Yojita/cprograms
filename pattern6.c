2331485d92ae8b6d54caf58256cc1ca053630462
yojita <yojita98@gmail.com>
2021-10-05 16:03:24 +0530

    Write a program to print AaBbCcDd........Zz

---
+++ pattern6.c
@@ -0,0 +1,25 @@
+/* Write a program to print following pattern 
+12345
+23456
+34567
+45678
+56789 */
+
+#include <stdio.h>
+int main()
+{
+    int rows;
+
+    printf("Enter the number of rows : ");
+    scanf("%d", &rows);
+    
+    for(int i = 1; i <= rows; i++)
+    {
+        for(int j = i; j < rows + i; j++)
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
